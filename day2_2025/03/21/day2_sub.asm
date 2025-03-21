c8239e61025d4bc99febe80e193eb01a73a6ef23
GMPBhagya <139644464+GMPBhagya@users.noreply.github.com>
2025-03-21 05:09:39 +0000

    sub.asm file created

---
+++ day2_2025/03/21/day2_sub.asm
@@ -0,0 +1,25 @@
+;Write an assembly program to substraction two numbbers and display the result
+.model small
+.stack 100h
+
+.data
+n db 2
+m db 3
+
+.code 
+start:
+
+mov ax,@data
+mov ds,ax
+
+mov dl,m ;dl=3
+sub dl,n ;dl=3-2=1
+add dl,48 ;dl=1+48=49
+
+mov ah,02h
+int 21h
+
+mov ax,4c00h
+int 21h
+
+end start
