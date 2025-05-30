5253aca750c442e5db76e4e87ff894e0d61a4872
GMPBhagya <139644464+GMPBhagya@users.noreply.github.com>
2025-05-30 04:48:38 +0000

    use a loop to dispaly number

---
+++ day_09/forloop02.asm
@@ -0,0 +1,36 @@
+;use a loop to display number from 1 to 5(e.g 1 2 3 4 5)
+.model small
+.stack 100h
+.data
+s db " $ "
+
+.code
+main:
+
+mov ax,@data
+mov ds,ax
+
+mov cl,5  ;loop 5 times
+mov bl,1  ;starting number=1
+
+print_loop:
+mov ah,02h
+mov dl,bl
+add dl,48
+int 21h  ;bl=1  bl=2
+
+mov dx,offset s
+mov ah,09h
+int 21h
+
+mov dl,10
+mov ah,02h
+int 21h
+
+inc bl
+loop print_loop
+
+mov ax,4c00h
+int 21h
+
+end main
